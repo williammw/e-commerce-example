182c2215fa5a33fd743e3eb0d2b6ac7154e01afa
WONG <wmwwong@legco.hksar>
2020-05-26 12:32:58 +0800

    Updates

---
+++ precache-manifest.94e0cf2ba9060bb8c7f2ba1d0ff87c35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4237b7b3abc664b41cb6fe879d0cd592",
+    "url": "/e-commerce-example/index.html"
+  },
+  {
+    "revision": "b71ee87a722e9b76d3c1",
+    "url": "/e-commerce-example/static/css/main.1daf46ae.chunk.css"
+  },
+  {
+    "revision": "21ff088ff0d0ae2eabc8",
+    "url": "/e-commerce-example/static/js/2.de78be02.chunk.js"
+  },
+  {
+    "revision": "197cea1212ba325d6dfc411e943034a5",
+    "url": "/e-commerce-example/static/js/2.de78be02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71ee87a722e9b76d3c1",
+    "url": "/e-commerce-example/static/js/main.8f4df044.chunk.js"
+  },
+  {
+    "revision": "b36892fdbc54a28cb9cb",
+    "url": "/e-commerce-example/static/js/runtime-main.573f0cab.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce-example/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-commerce-example/static/media/shopping-bag.30f07071.svg"
+  }
+]);
