68ead9cdd9bcab27cbf3b79a672bdff15727b4fb
wmwwong <wmwwong@legco.gov.hk>
2020-05-27 08:54:51 +0800

    user cart selector

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -6,8 +6,12 @@ import {ReactComponent as Logo } from '../../assets/crown.svg';
 
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selector';
+import { selectCurrentUser } from '../../redux/user/user.selector';
+
 
 const Header = ({currentUser, hidden}) => (
     <div className='header'>
--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -1,18 +1,32 @@
 import { createSelector } from 'reselect';
 
+// input selector
 const selectCart = state => state.cart;
+
 
 export const selectCartItems = createSelector(
   [selectCart],
   cart => cart.cartItems
 );
 
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
+
+/* 
+first : an array input select
+second : function of each output of input selector
+*/
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>
     cartItems.reduce(
+      //reduce
       (accumalatedQuantity, cartItem) =>
         accumalatedQuantity + cartItem.quantity,
       0
     )
 );
+
