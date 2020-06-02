fcfa8f8469b781b7538e34c91f74d689a0fb0f75
wmwwong <wmwwong@legco.gov.hk>
2020-06-02 17:14:40 +0800

    shop selector

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './collection.styles.scss'
+
+
+// import CollectionItem from '../../components/collection-item/collection-item.component'
+
+const CollectionPage = ({match}) => (
+    <div className='collectionpage' >
+        <h2>CollectionPage</h2>
+    </div>
+)
+
+export default CollectionPage
