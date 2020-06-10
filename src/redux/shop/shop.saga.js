bfe2fc8c82c865f8dc01730af16efeb355933a4b
wmwwong <wmwwong@legco.gov.hk>
2020-06-10 10:54:26 +0800

    redux-saga explaination

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,19 +1,21 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
-
-import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
-
+import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
+import { fetchCollectionsFailure, fetchCollectionsSuccess } from './shop.actions';
 import ShopActionTypes from './shop.types';
+
 
 export function* fetchCollections() {
   try {
     const collectionRef = firestore.collection('collections');
+    //yield means return a resolved promise object
     const snapshot = yield collectionRef.get();
+    // yield call() : in case theFetching is longer then expected
     const collectionsMap = yield call(
       convertCollectionsSnapshotToMap,
       snapshot
     );
+    //yield put == dispatch action
     yield put(fetchCollectionsSuccess(collectionsMap));
   } catch (error) {
     yield put(fetchCollectionsFailure(error.message));
